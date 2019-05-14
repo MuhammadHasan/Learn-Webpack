ebb0d84a68222bc53bb056344d5897dd78948319
MuhammadHasan <hasan.idrees@outlook.com>
2019-05-14 17:08:30 +0500

    Learn Webpack Cache Busting & Plugins (html-webpack-plugin)

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,22 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require("path");
+
 module.exports = {
     mode: "development",
-    // devtool: "none",
     entry: "./src/index.js",
-    output:{
-        filename: "main.js",
+    output: {
+        filename: "main.[contentHash].js",
         path: path.resolve(__dirname, "dist")
     },
-    module:{
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
+    module: {
         // css-loader take your css & turn it into javascript.
         // style-loader which take javascript & inject css to the DOM with <style>
-        rules: [
-            {
+        rules: [{
                 test: /\.css$/,
                 use: ["style-loader", "css-loader"]
             },
