f95e44a5b4df924d76f856a3cf31d2b50dc5e73e
MuhammadHasan <hasan.idrees@outlook.com>
2019-05-14 16:29:22 +0500

    Learn Webpack Loaders, Css & Sass

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { msg } from "./script1";
 import "./main.css";
+import "./main.scss";
 
 alert(msg);
 
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,14 @@ module.exports = {
             {
                 test: /\.css$/,
                 use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader", // inject style into DOM
+                    "css-loader", // convert css into js
+                    "sass-loader" // convert sass into css
+                ]
             }
         ]
     }
